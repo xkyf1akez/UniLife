639e3ced9f0c6ec4220a52ba7e276ce9eaf36ff4
XiTi <jcn30@bath.ac.uk>
2018-02-23 04:15:02 +0000

    Second Push
    Created the elements for:
    -Login
    -Signup
    -Settings
    -New Event
    -Home Page
    
    NOT STARTED:
    1) Friends List
    2) Calendar
    
    Need to:
    - refine the aethetics
    - add advanced functionalities (spinners, button navigation, icons)
    - add button funcationalities

--- UniLife-v1/app/src/main/java/integratedproject/unilife_v1/LoginActivity.java
+++ UniLife-v1/app/src/main/java/integratedproject/unilife_v1/LoginActivity.java
@@ -2,6 +2,11 @@ package integratedproject.unilife_v1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner ;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
+
