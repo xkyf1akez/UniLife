082442b9f92d41e4516c047b1c3515420ece3531
sr899 <sr899@bath.ac.uk>
2018-03-18 21:52:22 +0000

    Update RegistrationActivity.java

--- UniLife-v1/app/src/main/java/integratedproject/unilife_v1/RegistrationActivity.java
+++ UniLife-v1/app/src/main/java/integratedproject/unilife_v1/RegistrationActivity.java
@@ -4,6 +4,8 @@ package integratedproject.unilife_v1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -14,15 +16,47 @@ import java.util.Map;
 
 public class RegistrationActivity extends AppCompatActivity implements onTaskCompleted {
     private JSONParser results;
+    private Button register;
+    private TextView getEmail;
+    private TextView getPassword;
+    private TextView getConfirmation;
+    private TextView getForename;
+    private TextView getSurname;
+    private TextView getDepartment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.signup );
+        register = (Button)findViewById(R.id.button4);
 
-        Map map = new HashMap();
-        map.put("queryType", "getAllUsers");
-        new Database(this).execute(map);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String details = getDetails();
+                onTaskCompleted(details);
+                Map map = new HashMap();
+                map.put("register", "getAllUsers");
+                new Database(this).execute(map);
+            }
+        });
+    }
+
+    public String getDetails(){
+        getEmail = (TextView)findViewById(R.id.editText5);
+        String email = getEmail.getText().toString();
+        getPassword = (TextView)findViewById(R.id.editText8);
+        String password = getPassword.getText().toString();
+        getConfirmation = (TextView)findViewById(R.id.editText2);
+        String confirmation = getConfirmation.getText().toString();
+        getForename = (TextView)findViewById(R.id.editText2);
+        String forename = getForename.getText().toString();
+        getSurname = (TextView)findViewById(R.id.editText6);
+        String surname = getSurname.getText().toString();
+        getDepartment = (TextView)findViewById(R.id.editText6);
+        String department = getDepartment.getText().toString();
+        String details = email + " " + password + " " + confirmation + " " + forename + " " + surname + " " + department;
+        return details;
     }
 
     public void onTaskCompleted(String result) throws JSONException{
@@ -38,3 +72,4 @@ public class RegistrationActivity extends AppCompatActivity implements onTaskCom
     }
 
 }
+
