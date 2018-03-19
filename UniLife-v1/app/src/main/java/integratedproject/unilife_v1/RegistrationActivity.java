946fff7a8186c751f687c84552db4acaf5d93514
sr899 <sr899@bath.ac.uk>
2018-03-19 17:33:47 +0000

    Validates details, before passing to dbThread

--- UniLife-v1/app/src/main/java/integratedproject/unilife_v1/RegistrationActivity.java
+++ UniLife-v1/app/src/main/java/integratedproject/unilife_v1/RegistrationActivity.java
@@ -13,50 +13,59 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BooleanSupplier;
 
 public class RegistrationActivity extends AppCompatActivity implements onTaskCompleted {
     private JSONParser results;
     private Button register;
-    private TextView getEmail;
-    private TextView getPassword;
-    private TextView getConfirmation;
-    private TextView getForename;
-    private TextView getSurname;
-    private TextView getDepartment;
+    private TextView getValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup );
         register = (Button)findViewById(R.id.button4);
+        Database dbThread = new Database(this);
 
+        //sets up listener for button
         register.setOnClickListener(new View.OnClickListener() {
             @Override
+            //extracts values from textboxes and puts them through hash map with their appropriate key
+            //before passing it to the database thread
             public void onClick(View v) {
-                String details = getDetails();
-                onTaskCompleted(details);
-                Map map = new HashMap();
-                map.put("register", "getAllUsers");
-                new Database(this).execute(map);
+                Boolean validInputs = true;
+                //                {userName, password, confirmation, firstname, surname, department}
+                String[] values = { getDetails(R.id.editText5) , getDetails(R.id.editText8) , getDetails(R.id.editText2),
+                        getDetails(R.id.editText4) , getDetails(R.id.editText6) , getDetails(R.id.editText7) };
+                //ensures all user inputs are valid
+                for (int x; x < 6; x++){
+                    if(values[x] == null || values[x].isEmpty()) {
+                        validInputs = false;
+                        x = 6;
+                    }
+                }
+                //password confirmation
+                if (values[1] != values[2]) {
+                    validInputs = false;
+                }
+                if (validInputs == true) {
+                    Map map = new HashMap();
+                    map.put("userName", values[0]);
+                    map.put("password", values[1]);
+                    map.put("firstName", values[3]);
+                    map.put("surname", values[4]);
+                    map.put("department", values[5]);
+                    dbThread.execute(map);
+                }
             }
         });
     }
 
-    public String getDetails(){
-        getEmail = (TextView)findViewById(R.id.editText5);
-        String email = getEmail.getText().toString();
-        getPassword = (TextView)findViewById(R.id.editText8);
-        String password = getPassword.getText().toString();
-        getConfirmation = (TextView)findViewById(R.id.editText2);
-        String confirmation = getConfirmation.getText().toString();
-        getForename = (TextView)findViewById(R.id.editText2);
-        String forename = getForename.getText().toString();
-        getSurname = (TextView)findViewById(R.id.editText6);
-        String surname = getSurname.getText().toString();
-        getDepartment = (TextView)findViewById(R.id.editText6);
-        String department = getDepartment.getText().toString();
-        String details = email + " " + password + " " + confirmation + " " + forename + " " + surname + " " + department;
-        return details;
+    //gets user input from appropriate textbox
+    public String getDetails(int inputID){
+        getValue = (TextView)findViewById(inputID);
+        String value = getValue.getText().toString();
+        return value;
     }
 
     public void onTaskCompleted(String result) throws JSONException{
@@ -72,4 +81,3 @@ public class RegistrationActivity extends AppCompatActivity implements onTaskCom
     }
 
 }
-
