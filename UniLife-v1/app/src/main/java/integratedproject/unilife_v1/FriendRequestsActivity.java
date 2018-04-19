b61e849fbd738ffdf7379f0ccb9f9089901db873
sr899 <sr899@bath.ac.uk>
2018-04-19 12:23:26 +0100

    Update FriendRequestsActivity.java

--- UniLife-v1/app/src/main/java/integratedproject/unilife_v1/FriendRequestsActivity.java
+++ UniLife-v1/app/src/main/java/integratedproject/unilife_v1/FriendRequestsActivity.java
@@ -33,20 +33,49 @@ public class FriendRequestsActivity extends AppCompatActivity implements onTaskC
         map.put("queryType", "getFriendRequests");
         map.put("Username", User.getUsername()); 
         new Database(FriendRequestsActivity.this).execute(map);
+        
+        searchAdapter = new SearchAdapter(dataModel, getApplicationContext());
+        people.setAdapter(searchAdapter);
+        people.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SearchDataModel data = dataModel.get(position);
+
+                //when an item is clicked on, try sending a friend request to that person
+                Map map = new HashMap();
+                map.put("queryType", "respondFriendship");
+                map.put("username", User.getUsername());
+                map.put("responseType", 1);
+                new Database(SearchFriendsActivity.this).execute(map);
+            }
+        });
     }
 
     public void onTaskCompleted(String result) throws JSONException{
         results = new JSONParser(result);
         dataModel = new ArrayList<>();
 
-        if(results.getSuccess()) {
-            for(int i = 0; i < results.numOfResults(); i++) {
-                dataModel.add(new SearchDataModel(results.getString(i, "username"), results.getString(i, "firstName") + " " + results.getString(i, "surname"), results.getString(i, "department")));
+        if(results.getQueryType().equals("getUsers")) {
+            if (results.getSuccess()) {
+                //if successfully got users, add them to the dataModel and display in the listView
+                for (int i = 0; i < results.numOfResults(); i++) {
+                    dataModel.add(new SearchDataModel(results.getString(i, "username"), results.getString(i, "firstName") + " " + results.getString(i, "surname"), results.getString(i, "department")));
+                }
+                searchAdapter = new SearchAdapter(dataModel, getApplicationContext());
+                people.setAdapter(searchAdapter);
+
+            } else {
+                Toast.makeText(this, results.getMessage(), Toast.LENGTH_SHORT).show();
             }
-            searchAdapter = new SearchAdapter(dataModel, getApplicationContext());
-            people.setAdapter(searchAdapter);
+        } else if(results.getQueryType().equals("respondFriendship")) {
+            if(results.getSuccess()) {
+                //if successfully sent request, let the user know
+                Toast.makeText(this, results.getMessage(), Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, results.getMessage(), Toast.LENGTH_SHORT).show();
+            }
         } else {
-            Toast.makeText(this, results.getMessage(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Server error. Please try again", Toast.LENGTH_SHORT).show();
         }
 
     }
